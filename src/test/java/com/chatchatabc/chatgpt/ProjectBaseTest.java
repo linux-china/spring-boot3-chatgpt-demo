dc75433f463130fc98279d2e7c23c94f99bb2271
Libing Chen <libing.chen@gmail.com>
2023-05-01 00:11:34 +0800

    build: add @ActiveProfiles("test")

--- src/test/java/com/chatchatabc/chatgpt/ProjectBaseTest.java
+++ src/test/java/com/chatchatabc/chatgpt/ProjectBaseTest.java
@@ -1,7 +1,9 @@
 package com.chatchatabc.chatgpt;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public abstract class ProjectBaseTest {
 }
