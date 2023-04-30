7157cd0d25e0eb548315a45906fc5a8a17361436
Libing Chen <libing.chen@gmail.com>
2023-04-30 23:59:42 +0800

    app(boot): ChatGPTRobotApp

---
+++ src/main/java/com/chatchatabc/chatgpt/ChatGPTRobotApp.java
@@ -0,0 +1,13 @@
+package com.chatchatabc.chatgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatGPTRobotApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatGPTRobotApp.class, args);
+    }
+
+}
