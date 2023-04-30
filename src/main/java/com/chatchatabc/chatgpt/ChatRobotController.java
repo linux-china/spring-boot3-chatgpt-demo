91c7ae4f2c5f65062d63e83f5c8043368cc14e32
Libing Chen <libing.chen@gmail.com>
2023-04-30 23:59:17 +0800

    app: ChatRobotController

---
+++ src/main/java/com/chatchatabc/chatgpt/ChatRobotController.java
@@ -0,0 +1,22 @@
+package com.chatchatabc.chatgpt;
+
+
+import com.chatchatabc.chatgpt.chat.ChatCompletionRequest;
+import com.chatchatabc.chatgpt.chat.ChatGPTService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class ChatRobotController {
+    @Autowired
+    private ChatGPTService chatGPTService;
+
+    @PostMapping("/chat")
+    public Mono<String> chat(@RequestBody String content) {
+        return chatGPTService.chat(ChatCompletionRequest.of(content))
+                .map(chatCompletionResponse -> chatCompletionResponse.getReply().content());
+    }
+}
