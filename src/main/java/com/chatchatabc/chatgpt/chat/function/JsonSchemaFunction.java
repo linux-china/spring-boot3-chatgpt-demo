5bd64569085e0bbc5abc7f447a1e5bf80ebc179a
Libing Chen <libing.chen@gmail.com>
2023-06-16 15:00:26 +0800

    app: JsonSchemaFunction

---
+++ src/main/java/com/chatchatabc/chatgpt/chat/function/JsonSchemaFunction.java
@@ -0,0 +1,72 @@
+package com.chatchatabc.chatgpt.chat.function;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class JsonSchemaFunction {
+    private String name;
+    private String description;
+    private Parameters parameters;
+    @JsonIgnore
+    private Method javaMethod;
+
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    public record Parameters(String type, Map<String, JsonSchemaProperty> properties, List<String> required) {
+
+
+    }
+
+    public record JsonSchemaProperty(@JsonIgnore String name, String type, String description) {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Parameters getParameters() {
+        return parameters;
+    }
+
+    public void setParameters(Parameters parameters) {
+        this.parameters = parameters;
+    }
+
+    public Method getJavaMethod() {
+        return javaMethod;
+    }
+
+    public void setJavaMethod(Method javaMethod) {
+        this.javaMethod = javaMethod;
+    }
+
+    public void addProperty(String name, String type, String description) {
+        if (this.parameters == null) {
+            this.parameters = new Parameters("object", new HashMap<>(), new ArrayList<>());
+        }
+        this.parameters.properties.put(name, new JsonSchemaProperty(name, type, description));
+    }
+
+    public void addRequired(String name) {
+        this.getParameters().required.add(name);
+    }
+}
